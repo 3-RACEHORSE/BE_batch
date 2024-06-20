a6e8b2fe0ddd567993f55fa8b835d5e9c1f5525f
LEEForgiveness <121328409+LEEForgiveness@users.noreply.github.com>
2024-06-20 21:18:29 +0900

    feat: topic이름 변경(#8)

--- src/main/java/com/meetplus/batch/kafka/Topics.java
+++ src/main/java/com/meetplus/batch/kafka/Topics.java
@@ -15,7 +15,7 @@ public enum Topics {
     public static class Constant {
         public static final String NOTIFICATION_SERVICE = "alarm-topic";
         public static final String CHAT_SERVICE = "chat-topic";
-        public static final String AUCTION_POST_SERVICE = "new-auction-post-topic";
+        public static final String AUCTION_POST_SERVICE = "auction-post-topic";
         public static final String PAYMENT_SERVICE = "event-preview-topic";
     }
 
