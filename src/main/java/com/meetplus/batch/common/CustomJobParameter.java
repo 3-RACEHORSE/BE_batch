e7ab8b8861457ae0bb837e9bf43eebda1b0c79c4
ddolboghi <rhdfyd128@gmail.com>
2024-06-24 03:28:30 +0900

    refactor: 커스텀 잡 파라미터가 값을 주입 받도록 수정

--- src/main/java/com/meetplus/batch/common/CustomJobParameter.java
+++ src/main/java/com/meetplus/batch/common/CustomJobParameter.java
@@ -6,10 +6,22 @@ import org.springframework.beans.factory.annotation.Value;
 
 @Getter
 public class CustomJobParameter {
-    @Value("#{jobParameters['startTime']}")
-    private LocalDateTime startTime = DateRangeUtil.getStartTime();
-    @Value("#{jobParameters['endTime']}")
-    private LocalDateTime endTime = DateRangeUtil.getEndTime();
+    private LocalDateTime paymentJobStartTime;
+    private LocalDateTime paymentJobEndTime;
+
+    @Value("#{jobParameters['paymentJobStartTime']}")
+    public void setPaymentJobStartTime(String paymentJobStartTime) {
+        if(paymentJobStartTime != null && !paymentJobStartTime.isEmpty()) {
+            this.paymentJobStartTime = LocalDateTime.parse(paymentJobStartTime);
+        }
+    }
+
+    @Value("#{jobParameters['paymentJobEndTime']}")
+    public void setPaymentJobEndTime(String paymentJobEndTime) {
+        if(paymentJobEndTime != null && !paymentJobEndTime.isEmpty()) {
+            this.paymentJobEndTime = LocalDateTime.parse(paymentJobEndTime);
+        }
+    }
 }
 
 
