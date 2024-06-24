473a6d707326d26e7877429804fc70ccd61e67a7
ddolboghi <rhdfyd128@gmail.com>
2024-06-24 13:36:19 +0900

    feat: Jpa auditing 설정 추가

---
+++ src/main/java/com/meetplus/batch/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.meetplus.batch.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
