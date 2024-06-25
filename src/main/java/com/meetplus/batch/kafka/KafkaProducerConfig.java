d827a594537135936f62e57004c38ac5a6d254f4
LEEForgiveness <121328409+LEEForgiveness@users.noreply.github.com>
2024-06-25 09:42:01 +0900

    feat: kafka 토픽 자동 삭제 설정(#15)

--- src/main/java/com/meetplus/batch/kafka/KafkaProducerConfig.java
+++ src/main/java/com/meetplus/batch/kafka/KafkaProducerConfig.java
@@ -1,12 +1,16 @@
 package com.meetplus.batch.kafka;
 
+import com.meetplus.batch.kafka.Topics.Constant;
 import java.util.HashMap;
 import java.util.Map;
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.config.TopicConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -16,6 +20,24 @@ import org.springframework.kafka.support.serializer.JsonSerializer;
 public class KafkaProducerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootstrapAddress;
+
+    @Bean
+    public NewTopic myTopic() {
+        return TopicBuilder.name(Constant.ALARM_TOPIC)
+            .partitions(1)
+            .replicas(1)
+            .config(TopicConfig.RETENTION_MS_CONFIG, String.valueOf(86400000)) // 1일 (24시간) = 86400000 밀리초
+            .build();
+    }
+
+    @Bean
+    public NewTopic myTopic2() {
+        return TopicBuilder.name(Topics.Constant.AUCTION_POST_DONATION_UPDATE)
+            .partitions(1)
+            .replicas(1)
+            .config(TopicConfig.RETENTION_MS_CONFIG, String.valueOf(86400000)) // 1일 (24시간) = 86400000 밀리초
+            .build();
+    }
 
     @Bean
     public ProducerFactory<String, Object> producerFactory() {
