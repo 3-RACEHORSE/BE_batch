fe9de9a06b05adbf27a3d4766fcd106f7594f2a8
ddolboghi <rhdfyd128@gmail.com>
2024-07-06 17:30:13 +0900

    refactor: ItemReader StepScope로 수정

--- src/main/java/com/meetplus/batch/job/PaymentCancelJob.java
+++ src/main/java/com/meetplus/batch/job/PaymentCancelJob.java
@@ -9,6 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
@@ -46,7 +47,7 @@ public class PaymentCancelJob {
     }
 
     @Bean
-    @JobScope
+    @StepScope
     public PaymentCancelItemReader paymentCancelReader() {
         return new PaymentCancelItemReader(paymentRepository, customJobParameter);
     }
